35ec0d3a14ae8295ddf08d049a289841bcb2584b
Cheikh melainine <Cheikhmelaininehaimidaha@gmail.com>
2023-03-04 21:16:49 +0100

    first commit

---
+++ src/generique/DAO.java
@@ -0,0 +1,15 @@
+package generique;
+
+import java.util.List;
+
+
+public interface DAO<T> {
+	
+	void save(T p);
+	void remove(T p);
+	void Update(T p);
+	void FindPerson(T p);
+	List<T> getAllAvion();
+	void afficherall();
+
+}
